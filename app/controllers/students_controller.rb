dcbccf006157681f6eeb0883a8ea0ae594a8ec7f
Nicholas Rostov <jonathan.w.coll@gmail.comgit config --global user.email jonathan.w.coll@gmail.com>
2017-03-09 21:29:39 -0500

    displaying data

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,9 +1,12 @@
 class StudentsController < ApplicationController
 
   def show
-    @student = Unirest.get("localhost:3001/api/v1/students/#{params[:id]}").body
+    # @student = Unirest.get("localhost:3001/api/v1/students/#{params[:id]}").body
+    @student = Student.first
     render "show.html.erb"
   end
+
+
 
 
 end
